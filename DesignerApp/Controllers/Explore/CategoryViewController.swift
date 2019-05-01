c55491d9caf8a85fe5ee1be5370588d25a027c45
Moe <mohamadbajuaifer@gmail.com>
2019-05-01 17:51:34 +0300

    Add Categories VC

--- DesignerApp/Controllers/Explore/CategoryViewController.swift
+++ DesignerApp/Controllers/Explore/CategoryViewController.swift
@@ -18,8 +18,8 @@ class CategoryViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .white
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.backgroundColor = #colorLiteral(red: 0.4151936173, green: 0.412730217, blue: 0.4170902967, alpha: 1)
+        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     init() {
@@ -36,12 +36,18 @@ class CategoryViewController: UICollectionViewController {
 extension CategoryViewController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 3
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let designsListController = DesignsListController()
+        
+        self.navigationController?.pushViewController(designsListController, animated: true)
     }
 }
 
@@ -50,6 +56,28 @@ extension CategoryViewController {
 extension CategoryViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width, height: 44)
+        
+        let leftRightPadding = view.frame.width  * 0.02
+        let interSpacing = view.frame.width * 0.02
+        let cellWidth = (view.frame.width - 2 * leftRightPadding - 2 * interSpacing) / 2
+        print(cellWidth)
+        return .init(width: cellWidth, height: cellWidth - 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        let leftRightPadding = view.frame.width  * 0.02
+        print(leftRightPadding)
+        return .init(top: 20, left: leftRightPadding, bottom: 10, right: leftRightPadding)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        let interSpacing = view.frame.width * 0.02
+        return interSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
     }
 }
