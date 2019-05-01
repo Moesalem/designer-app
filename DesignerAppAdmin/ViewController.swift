c49e8c3a1433c028b7513b050de5f27c1c8417c9
Moe <mohamadbajuaifer@gmail.com>
2019-05-01 17:54:01 +0300

    new target

---
+++ DesignerAppAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignerAppAdmin
+//
+//  Created by Moe on 01/05/2019.
+//  Copyright © 2019 Mohammed salem bajuaifer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
