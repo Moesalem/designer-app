ae59636e90f04bd0d9e179c172b5c94dd4f8dd47
Moe <mohamadbajuaifer@gmail.com>
2019-05-11 18:17:28 +0300

    Initial Designs/Products View + fetching from firestore

--- DesignerApp/Models/Product.swift
+++ DesignerApp/Models/Product.swift
@@ -7,6 +7,21 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 struct Product {
+    var name, id, category, imgUrl: String
+    var price: Double
+    var timeStamp: Timestamp
+    var stock: Int
+    
+    init(data: [String: Any]) {
+        self.name = data["name"] as? String ?? ""
+        self.id = data["id"] as? String ?? ""
+        self.category = data["category"] as? String ?? ""
+        self.price = data["price"] as? Double ?? 9.99
+        self.stock = data["stock"] as? Int ?? 1
+        self.imgUrl = data["imgUrl"] as? String ?? ""
+        self.timeStamp = data["timestamp"] as? Timestamp ?? Timestamp()
+    }
 }
--- DesignerApp/Views/DesignCell.swift
+++ DesignerApp/Views/DesignCell.swift
@@ -10,10 +10,35 @@ import UIKit
 
 class DesignCell: UICollectionViewCell {
     
+    
+    var designImage = UIImageView(image: #imageLiteral(resourceName: "Rectangle"))
+    let designLabel = UILabel(text: "Banners", font: .boldSystemFont(ofSize: 24.adjusted))
+    let shadowView = UIView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         // UI Setup
+        designImage.contentMode = .scaleAspectFill
+        designImage.clipsToBounds = true
+        designImage.layer.cornerRadius = 8.adjusted
+        
+        designLabel.textColor = .white
+        designLabel.textAlignment = .center
+        
+        shadowView.layer.cornerRadius = 8.adjusted
+        shadowView.layer.shadowRadius = 8.adjusted
+        shadowView.layer.shadowColor = UIColor.black.cgColor
+        shadowView.layer.shadowOpacity = 0.4
+        
+        addSubview(shadowView)
+        shadowView.addSubview(designImage)
+        shadowView.fillSuperview()
+        designImage.fillSuperview()
+        shadowView.addSubview(designLabel)
+        
+        designLabel.anchor(top: nil, leading: shadowView.leadingAnchor, bottom: shadowView.bottomAnchor, trailing: shadowView.trailingAnchor)
+        designLabel.constrainHeight(constant: 50.adjusted)
     }
     
     required init?(coder aDecoder: NSCoder) {
