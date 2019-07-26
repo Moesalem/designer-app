02992a90c196c43300567c3b9945bb0f790990d8
Moe <mohamadbajuaifer@gmail.com>
2019-07-26 21:51:15 +0300

    Snapping Horizontal Controller

---
+++ DesignerApp/Controllers/HorizontalController.swift
@@ -0,0 +1,47 @@
+//
+//  HorizontalCollectionView.swift
+//  DesignerApp
+//
+//  Created by Moe on 26/07/2019.
+//  Copyright © 2019 Mohammed salem bajuaifer. All rights reserved.
+//
+
+import UIKit
+
+class HorizontalController: UICollectionViewController {
+    
+    init() {
+        let layout = SnappingCollectionViewLayout()
+        layout.scrollDirection = .horizontal
+        super.init(collectionViewLayout: layout)
+        collectionView.decelerationRate = .fast
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class SnappingCollectionViewLayout: UICollectionViewFlowLayout {
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
+        let parent = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+        let itemWidth = collectionView.frame.width - 48
+        let itemSpace = itemWidth + minimumInteritemSpacing
+        var currentItemIdx = round(collectionView.contentOffset.x / itemSpace)
+        
+        // Skip to the next cell, if there is residual scrolling velocity left.
+        // This helps to prevent glitches
+        let vX = velocity.x
+        if vX > 0 {
+            currentItemIdx += 1
+        } else if vX < 0 {
+            currentItemIdx -= 1
+        }
+        
+        let nearestPageOffset = currentItemIdx * itemSpace
+        return CGPoint(x: nearestPageOffset,
+                       y: parent.y)
+    }
+}
